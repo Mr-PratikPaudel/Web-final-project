dfdd179930abd4b432e43702021eceb954a5ee07
Subekshya-KC <sububoom2@gmail.com>
2026-03-16 11:09:49 +0545

    accounts added

---
+++ eventflow/accounts/serializers.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from .models import UserProfile
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ['phone', 'bio', 'avatar_initials']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    profile = UserProfileSerializer(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'profile']
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name', 'password', 'password2']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({'password': 'Passwords do not match.'})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = User.objects.create_user(**validated_data)
+        UserProfile.objects.create(user=user)
+        return user
--- eventflow/accounts/views.py
+++ eventflow/accounts/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.models import User
+from .models import UserProfile
 
-# Create your views here.
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('event_list')
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            next_url = request.GET.get('next', '/')
+            return redirect(next_url)
+        else:
+            messages.error(request, 'Invalid username or password.')
+
+    return render(request, 'accounts/login.html')
+
+
+def register_view(request):
+    if request.user.is_authenticated:
+        return redirect('event_list')
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+
+        if password != password2:
+            messages.error(request, 'Passwords do not match.')
+        elif User.objects.filter(username=username).exists():
+            messages.error(request, 'Username already taken.')
+        elif User.objects.filter(email=email).exists():
+            messages.error(request, 'Email already registered.')
+        else:
+            user = User.objects.create_user(
+                username=username, email=email,
+                first_name=first_name, last_name=last_name,
+                password=password
+            )
+            UserProfile.objects.create(user=user)
+            login(request, user)
+            messages.success(request, 'Welcome to EventFlow!')
+            return redirect('event_list')
+
+    return render(request, 'accounts/register.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
