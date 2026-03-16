30733b66c86d35fdc4081af6921d56ff344d02ff
Laxmi Saud <lsaud6620@gmail.com>
2026-03-16 13:21:36 +0545

    events updated

--- eventflow/events/admin.py
+++ eventflow/events/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Event, Booking, Category
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'icon', 'color']
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['title', 'organizer', 'category', 'start_date', 'status', 'capacity']
+    list_filter = ['status', 'category']
+    search_fields = ['title', 'venue', 'city']
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ['booking_reference', 'attendee', 'event', 'seats', 'status', 'booked_at']
+    list_filter = ['status']
+    search_fields = ['booking_reference', 'attendee__username']
